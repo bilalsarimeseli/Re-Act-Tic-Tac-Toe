842f7af5b5399130339eb13bc9538f88b3092bf7
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-03-10 13:38:41 -0700

    manifest start url changed

---
+++ build/precache-manifest.eeb5eafee78f3b60798b0e3f54e054f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0813a370923d7c88926acad1f6671b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e23a18fac5892e8f3b1",
+    "url": "/static/css/main.49d99dbd.chunk.css"
+  },
+  {
+    "revision": "b139e5fdec88ee276725",
+    "url": "/static/js/2.d793e4b2.chunk.js"
+  },
+  {
+    "revision": "9e23a18fac5892e8f3b1",
+    "url": "/static/js/main.959a5e1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
