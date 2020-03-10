d58bcfba3386efd09913564de9bb7ed1af6f7f37
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-03-10 13:33:48 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bilalsarimeseli/Re-Act-Tic-Tac-Toe.git/precache-manifest.ae6fc0773a6f773f70e81785b0c10eb5.js"
+  "/precache-manifest.eeb5eafee78f3b60798b0e3f54e054f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bilalsarimeseli/Re-Act-Tic-Tac-Toe.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
